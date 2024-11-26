5f9d8e2d3cb0cdd15a7b7bf931c6e2096f6f8910
Karen <julieth.sanmiguel@espoch.edu.ec>
2024-11-26 10:49:31 -0500

    comentario actividad

---
+++ src/main/java/com/mycompany/paquete/compraspublicasespoch/ComprasPublicasEspoch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.paquete.compraspublicasespoch;
+
+/**
+ *
+ * @author DELL
+ */
+public class ComprasPublicasEspoch {
+
+    public static void main(String[] args) {
+        System.out.println("Good morning");
+    }
+}
